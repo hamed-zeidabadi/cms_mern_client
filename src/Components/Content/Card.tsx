82cc604e63a9f082b086de6ca886ab37b9714971
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-12-09 12:06:19 +0330

    add style in componnets => card

--- src/Components/Content/Card.tsx
+++ src/Components/Content/Card.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import "./Card.scss";
 export interface CardProps {}
 
 const Card: React.FC<CardProps> = () => {
   return (
     <>
-      <div className="card"></div>
+      <div classNameName="card">
+        <main className="page-content">
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Mountain View</h2>
+              <p className="copy">
+                Check out all of these gorgeous mountain trips with beautiful
+                views of, you guessed it, the mountains
+              </p>
+              <button className="btn">View Trips</button>
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
