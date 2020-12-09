08a15afbe71fac4d72f2a7f4455da63164532f09
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-12-09 12:23:10 +0330

    Fix style in componnets => card

--- src/Components/Content/Card.tsx
+++ src/Components/Content/Card.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Card.scss";
+import bg from "../../images/test.jpg";
+import { url } from "inspector";
 export interface CardProps {}
 
 const Card: React.FC<CardProps> = () => {
   return (
     <>
-      <div classNameName="card">
+      <div className="card_main">
         <main className="page-content">
-          <div className="card">
+          <div className="card" style={{ backgroundImage: `url(${bg})`  }}>
             <div className="content">
-              <h2 className="title">Mountain View</h2>
+              <h2 className="title">آموزش جاوا اسکریپت</h2>
               <p className="copy">
-                Check out all of these gorgeous mountain trips with beautiful
-                views of, you guessed it, the mountains
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که ...
               </p>
-              <button className="btn">View Trips</button>
+              <button className="btn">خواندن مقاله</button>
             </div>
           </div>
         </main>
